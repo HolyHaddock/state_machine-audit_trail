1765775d1d1e9febbe8f391164d46975544d4a4a
Mark Harris <holyhaddock@gmail.com>
2015-02-27 15:30:17 +0000

    Finished the integration with mongoid.

--- lib/state_machine/audit_trail/backend/mongoid.rb
+++ lib/state_machine/audit_trail/backend/mongoid.rb
@@ -17,10 +17,12 @@ class StateMachine::AuditTrail::Backend::Mongoid < StateMachine::AuditTrail::Bac
   # from:   the state of the object prior to the event
   # to:     the state of the object after the event
   def log(object, event, from, to, timestamp = Time.now)
-    foreign_key_field = owner_class.relations.keys.first
-    params = {foreign_key_field => object, :event => event, :from => from, :to => to, :created_at => timestamp}
+    owner_key_field = owner_class.relations.keys.first
+    transition_key_field = transition_class.relations.keys.first
+    params = {:event => event, :from => from, :to => to, :created_at => timestamp}
     [context_to_log].flatten(1).each { |context| params[context] = object.send(context) } unless self.context_to_log.nil?
-    object.send(foreign_key_field) << transition_class.new(params)
+    new_transition = object.send(owner_key_field).build(params)
+    new_transition.save!
   end
 
 end
